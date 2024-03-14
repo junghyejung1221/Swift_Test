c34730806cda64b66a4a0328defe3856e94cb947
Lucia <jung991221@naver.com>
2024-03-14 19:13:56 +0900

    commit

---
+++ test7_gitgitgit/test7_gitgitgit/test7_gitgitgitApp.swift
@@ -0,0 +1,17 @@
+//
+//  test7_gitgitgitApp.swift
+//  test7_gitgitgit
+//
+//  Created by 정혜정 on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct test7_gitgitgitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
